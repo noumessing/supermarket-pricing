f7e5c303ce9036d5cacda14682869c73be133b17
NOUMES <rolangnoumessing@yahoo.fr>
2022-07-08 06:19:57 +0100

    second Commit

--- supermarket/src/main/java/com/supermarket/dao/ProduitDaoImpl.java
+++ supermarket/src/main/java/com/supermarket/dao/ProduitDaoImpl.java
@@ -9,78 +9,95 @@ import java.util.List;
 import com.supermarket.model.Produit;
 
 public class ProduitDaoImpl implements ProduitDao {
-	
-	private static final String INSERT_PRODUIT_SQL = "INSERT INTO produit" + "  (nom_produit) VALUES " +
-	        " (?);";
 
-	    private static final String SELECT_PRODUIT_BY_ID = "select id,nom_produit from produit where id =?";
-	    private static final String SELECT_ALL_PRODUITS = "select * from produit";
-	    private static final String DELETE_PRODUITS_SQL = "delete from produit where id = ?;";
-	    private static final String UPDATE_PRODUITS_SQL = "update produit set nom_produit = ? where id = ?;";
-	    private static final String SELECT_NB_PRODUIT = "select count(*) from produit;";
+	private static final String INSERT_PRODUIT_SQL = "INSERT INTO produit" + "  (nom_produit) VALUES " + " (?);";
+
+	private static final String SELECT_PRODUIT_BY_ID = "select id,nom_produit from produit where id =?";
+	private static final String SELECT_ALL_PRODUITS = "select * from produit";
+	private static final String DELETE_PRODUITS_SQL = "delete from produit where id = ?;";
+	private static final String UPDATE_PRODUITS_SQL = "update produit set nom_produit = ? where id = ?;";
+	private static final String SELECT_NB_PRODUIT = "select count(*) from produit;";
 
 	public void insertProduit(Produit produit) {
-		Connection con=null;
-		 con =ConnectionDao.createconnection();
-		 try {
+		Connection con = null;
+		con = ConnectionDao.createconnection();
+		try {
 			PreparedStatement preparedStatement = con.prepareStatement(INSERT_PRODUIT_SQL);
 			preparedStatement.setString(1, produit.getNomProduit());
 			System.out.println(preparedStatement);
-            preparedStatement.executeUpdate();
-            	} catch (SQLException e) {
-		
-            			
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+
 			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		 
-		 
+
 	}
 
 	public Produit findProduit(long id) {
 
-		Connection con=null;
-		 con =ConnectionDao.createconnection();
-		 Produit produit = null;
-        
-       
-		 try { 
-            PreparedStatement preparedStatement = con.prepareStatement(SELECT_PRODUIT_BY_ID); 
-            
-				preparedStatement.setLong(1, id);
-			
-            System.out.println(preparedStatement);
-          
-            ResultSet rs = preparedStatement.executeQuery();
+		Connection con = null;
+		con = ConnectionDao.createconnection();
+		Produit produit = null;
 
-            while (rs.next()) {
-                String nomProduit = rs.getString(2);
-                
-                produit = new Produit(id, nomProduit);
-            }
-	} catch (SQLException e) { }
-        
+		try {
+			PreparedStatement preparedStatement = con.prepareStatement(SELECT_PRODUIT_BY_ID);
+
+			preparedStatement.setLong(1, id);
+
+			System.out.println(preparedStatement);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				String nomProduit = rs.getString(2);
+
+				produit = new Produit(id, nomProduit);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return produit;
 	}
 
 	public long nombreProduit() {
-		Connection con=null;
-		 con =ConnectionDao.createconnection();
-		 long nb=-1;
-		 try {
-			 PreparedStatement preparedStatement = con.prepareStatement(SELECT_NB_PRODUIT); 
+		Connection con = null;
+		con = ConnectionDao.createconnection();
+		long nb = -1;
+		try {
+			PreparedStatement preparedStatement = con.prepareStatement(SELECT_NB_PRODUIT);
 
-         ResultSet rs = preparedStatement.executeQuery();
+			ResultSet rs = preparedStatement.executeQuery();
 
-         while (rs.next()) {
-             nb=  rs.getLong(1);}
-           
-            	} catch (SQLException e) {
+			while (rs.next()) {
+				nb = rs.getLong(1);
+			}
+
+		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		 
-		 return nb;}
-	
+
+		return nb;
+	}
+
 	public List<Produit> selectAllProduit() {
 		// TODO Auto-generated method stub
 		return null;
--- supermarket/src/main/java/com/supermarket/metier/ProduitMetierImpl.java
+++ supermarket/src/main/java/com/supermarket/metier/ProduitMetierImpl.java
@@ -6,25 +6,20 @@ import com.supermarket.dao.ProduitDao;
 import com.supermarket.dao.ProduitDaoImpl;
 import com.supermarket.model.Produit;
 
-
 public class ProduitMetierImpl implements ProduitMetier {
-	
+
 	private ProduitDao produitDao;
 
 	public ProduitMetierImpl(ProduitDao produitDao) {
-		
+
 		this.produitDao = produitDao;
 	}
-	
-	
 
 	public ProduitMetierImpl() {
 		super();
-		this.produitDao= new ProduitDaoImpl();
-		
+		this.produitDao = new ProduitDaoImpl();
+
 	}
-
-
 
 	public void saveProduit(Produit p) {
 		// TODO Auto-generated method stub
@@ -32,21 +27,17 @@ public class ProduitMetierImpl implements ProduitMetier {
 	}
 
 	public List<Produit> listProduit() {
-	
+
 		return null;
 	}
-
-
 
 	public Produit findById(long id) {
 		// TODO Auto-generated method stub
 		return produitDao.findProduit(id);
 	}
 
-
-
 	public long nombreProduit() {
-		// TODO Auto-generated method stub
+		
 		return produitDao.nombreProduit();
 	}
 
